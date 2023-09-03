3f26532e474ab66d9d4ace8937bf051a2e5f8567
DreamLineLove <zwenyanzaw@protonmail.com>
2023-09-03 19:55:30 +0630

    Timed quiz that ends after 30 seconds on default

--- main.go
+++ main.go
@@ -8,14 +8,17 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 var csvName string
+var timelimit int
 var total int
 var total_correct int
 
 func main() {
-	flag.StringVar(&csvName, "filename", "problems.csv", "Give the name of the csv file.")
+	flag.StringVar(&csvName, "filename", "problems.csv", `Give the name of the csv file. Defaults to "problems.csv".`)
+	flag.IntVar(&timelimit, "timelimit", 30, "Give the time limit in seconds for the whole quiz. Defaults to 30.")
 	flag.Parse()
 
 	csvFile, err := os.Open(csvName)
@@ -30,19 +33,29 @@ func main() {
 		log.Fatal(err)
 	}
 
-	bufioRd := bufio.NewReader(os.Stdin)
-	for i, v := range questions {
-		fmt.Print(i+1, ". ", v[0], "\n")
-		ans, err := bufioRd.ReadString('\n')
-		if err != nil {
-			log.Fatal(err)
-		}
-		if strings.TrimSpace(ans) == v[1] {
-			total_correct++
-		}
-		total++
-		bufioRd.Reset(os.Stdin)
-	}
+	ch := make(chan bool)
 
-	fmt.Printf("\n\nTotal: %d\nCorrect: %d\n", total, total_correct)
+	go func() {
+		bufioRd := bufio.NewReader(os.Stdin)
+		for i, v := range questions {
+			fmt.Print(i+1, ". ", v[0], "\n")
+			ans, err := bufioRd.ReadString('\n')
+			if err != nil {
+				log.Fatal(err)
+			}
+			if strings.TrimSpace(ans) == v[1] {
+				total_correct++
+			}
+			total++
+			bufioRd.Reset(os.Stdin)
+		}
+		ch <- true
+	}()
+
+	select {
+	case <-ch:
+		fmt.Printf("\n\nTotal: %d\nCorrect: %d\n", total, total_correct)
+	case <-time.After(time.Second * time.Duration(timelimit)):
+		fmt.Print("\n\nTime ran out!\n")
+	}
 }
